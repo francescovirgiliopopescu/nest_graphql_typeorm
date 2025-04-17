b7ac6652f8703be755ecf3450fde14b9791fb2ba
francescovirgiliopopescu <francescovirgiliopopescu@gmail.com>
2025-04-17 14:08:43 -0700

    feat: update main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// add the comments
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
